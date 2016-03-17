dd1a962a3fa8bd93afa24ce2755e0c8fea8e37b4
Chris Mark <chdmark@gmail.com>
2016-03-16 22:27:00 -0700

    create soundpool for app

--- app/src/main/java/com/christopherdmark/playingsounds/MainActivity.java
+++ app/src/main/java/com/christopherdmark/playingsounds/MainActivity.java
@@ -1,22 +1,51 @@
 package com.christopherdmark.playingsounds;
 
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SoundPool soundPool;
+    int sample1 = -1;
+    int sample2 = -2;
+    int sample3 = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+
+        try{
+            AssetManager assetManager = getAssets();
+            AssetFileDescriptor descriptor;
+
+            descriptor = assetManager.openFd("sample1.ogg");
+            sample1 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("sample2.ogg");
+            sample2 = soundPool.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("sample3.ogg");
+            sample3 = soundPool.load(descriptor, 0);
+
+        }catch (IOException e){
+
+        }
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
